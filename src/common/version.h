1e192ae1fe342f938b9dc1ddd204eea19dc7c2d5
pFranklin <zhang19880129@gmail.com>
2011-02-08 15:00:27 +0800

    add version file
    Signed-off-by: pFranklin <zhang19880129@gmail.com>

---
+++ src/common/version.h
@@ -0,0 +1,22 @@
+/*============================================================================
+  Lightning - Cross Platform Distributed Building System
+  Copyright 2010-2011 Paul Franklin
+
+  Distributed under the GNU General Public License v2.0 License or later;
+  see accompanying file license for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the License for more information.
+============================================================================*/
+
+#ifndef LIGHTNING_VERSION_H
+#define LIGHTNING_VERSION_H
+
+
+#define LIGHTNING_VERSION "1_0"
+
+#endif
+
+
+
